c21be3c23c0b3bcc1d8576e6076fa60a561a4d53
Documenter.jl <documenter@juliadocs.github.io>
2024-03-24 17:21:25 +0000

    build based on ad4c6b2

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SymbolicNeuralNetworks","category":"page"},{"location":"#SymbolicNeuralNetworks","page":"Home","title":"SymbolicNeuralNetworks","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SymbolicNeuralNetworks.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SymbolicNeuralNetworks]","category":"page"}]
+}
