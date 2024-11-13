b4c2d9d02442e95837ea4bbfa50055c5fec142de
benedict-96 <benedikt.brantner@icloud.com>
2024-11-13 07:19:08 +0100

    Made SHNN a separate struct and added docstrings.

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -1,32 +1,51 @@
 """
-    HamiltonianSymbolicNeuralNetwork
+    HamiltonianSymbolicNeuralNetwork <: AbstractSymbolicNeuralNetwork
 
-symbolic_hamitonian is a functions that creates a symbolic hamiltonian from any hamiltonian.
-The output of the function is 
-- the symbolics parameters used to build the hamiltonian (i.e t, q, p),
-- the symbolic expression of the hamiltonian,
-- the function generated from the symbolic hamiltonian. 
+A struct that inherits properties from the abstract type `AbstractSymbolicNeuralNetwork`.
+
+# Constructor
+
+    HamiltonianSymbolicNeuralNetwork(model)
+
+Make an instance of `HamiltonianSymbolicNeuralNetwork` based on a `Chain` or an `Architecture`.
+This is similar to the constructor for [`SymbolicNeuralNetwork`](@ref) but also checks if the input dimension is even-dimensional and the output dimension is one.
 """
+struct HamiltonianSymbolicNeuralNetwork{AT, MT, PT} <: AbstractSymbolicNeuralNetwork{AT}
+    architecture::AT
+    model::MT
+    params::PT
+end
+
 function HamiltonianSymbolicNeuralNetwork(arch::Architecture, model::Model)
-
-    input_dim = input_dimension(model)
-    n = input_dim ÷ 2
-    @assert iseven(input_dim) "Input dimension has to be an even number."
+    @assert iseven(input_dimension(model)) "Input dimension has to be an even number."
     @assert output_dimension(model) == 1 "Output dimension of network has to be scalar."
-     
-    x = Symbolics.variables(:x, 1:input_dim)
-    ∇nn = Symbolics.variables(:∇nn, 1:input_dim)
-    nn = Symbolics.variables(:nn, 1:1)
 
+    sparams = symbolicparameters(model)
+    HamiltonianSymbolicNeuralNetwork(arch, model, sparams)
+end
+
+HamiltonianSymbolicNeuralNetwork(model::Model) = HamiltonianSymbolicNeuralNetwork(UnknownArchitecture(), model)
+HamiltonianSymbolicNeuralNetwork(arch::Architecture) = HamiltonianSymbolicNeuralNetwork(arch, Chain(model))
+
+"""
+    vector_field(nn::HamiltonianSymbolicNeuralNetwork)
+
+Get the symbolic expression for the vector field belonging to the HNN `nn`.
+
+# Implementation 
+
+This is calling [`gradient`](@ref) and then multiplies the result with a Poisson tensor.
+"""
+function vector_field(nn::HamiltonianSymbolicNeuralNetwork)
+    gradient_output = gradient(nn)
+    sinput, soutput, ∇nn = gradient_output.x, gradient_output.soutput, gradient_output.s∇output
+    input_dim = input_dimension(nn.model)
+    n = input_dim ÷ 2
     # placeholder for one
-    @variables o 
+    @variables o
     o_vec = repeat([o], n)
     𝕀 = Diagonal(o_vec)
     𝕆 = zero(𝕀)
     𝕁 = hcat(vcat(𝕆, -𝕀), vcat(𝕀, 𝕆))
-    eqs = (x = x, nn = nn, ∇nn = ∇nn, hvf = substitute(𝕁 * ∇nn, Dict(o => 1, )))
-
-    SymbolicNeuralNetwork(arch, model; eqs = eqs)
+    (x = sinput, nn = soutput, ∇nn = ∇nn, hvf = substitute(𝕁 * ∇nn, Dict(o => 1, )))
 end
-
-HamiltonianSymbolicNeuralNetwork(model::Model) = HamiltonianSymbolicNeuralNetwork(UnknownArchitecture(), model)
