022fa0fe99b87277b68d53aa87330d69d7081a3e
benedict-96 <benedikt.brantner@icloud.com>
2024-11-18 14:12:28 +0100

    Commented out almost all tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,12 @@ using SymbolicNeuralNetworks
 using SafeTestsets
 using Test
 
-@safetestset "Symbolic Neural network                                                                " begin include("neural_network_derivative.jl") end
+# @safetestset "Symbolic Neural network                                                                " begin include("neural_network_derivative.jl") end
 # @safetestset "Symbolic Params                                                                        " begin include("test_params.jl") end
 # @safetestset "Develop/Envelop                                                                        " begin include("test_de_envelop.jl") end
 # @safetestset "Get Track                                                                              " begin include("test_get_track.jl") end
 # @safetestset "Rewrite code                                                                           " begin include("test_rewrite.jl") end
-# @safetestset "Hamiltonian                                                                            " begin include("test_hamiltonian.jl") end
+@safetestset "HNN Loss                                                                               " begin include("test_hnn_loss_pullback.jl") end
 # @safetestset "Lagrangian                                                                             " begin include("test_lagrangian.jl") end
 #= To add once GeometricMachineLearning is updated on the main branch
 @safetestset "SymbolicNeuralNetwork                                                                  " begin include("test_symbolic_neuralnet.jl") end
