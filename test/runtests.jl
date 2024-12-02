2909f6bf1767d3399ec72c29bb2d2c6e52dc9d99
benedict-96 <benedikt.brantner@icloud.com>
2024-12-02 21:46:07 +0100

    Not using one of the tests in symbolic_gradient.jl for now.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using SafeTestsets
 using Test
 
 @safetestset "Docstring tests.                                                                      " begin include("doctest.jl") end
-# @safetestset "Symbolic gradient                                                                      " begin include("symbolic_gradient.jl") end
+@safetestset "Symbolic gradient                                                                      " begin include("symbolic_gradient.jl") end
 @safetestset "Symbolic Neural network                                                                " begin include("neural_network_derivative.jl") end
 @safetestset "Symbolic Params                                                                        " begin include("test_params.jl") end
 # @safetestset "HNN Loss                                                                               " begin include("test_hnn_loss_pullback.jl") end
--- test/symbolic_gradient.jl
+++ test/symbolic_gradient.jl
@@ -1,5 +1,5 @@
 using SymbolicNeuralNetworks
-using SymbolicNeuralNetworks: symbolic_differentials, symbolic_gradient, build_executable_gradient, _build_executable_gradient
+using SymbolicNeuralNetworks: symbolic_differentials, symbolic_derivative, _build_nn_function
 using LinearAlgebra: norm
 using Symbolics, AbstractNeuralNetworks
 using AbstractNeuralNetworks: NeuralNetworkParameters
@@ -16,17 +16,17 @@ function test_symbolic_gradient(input_dim::Integer = 3, output_dim::Integer = 1,
     c = Chain(Dense(input_dim, hidden_dim, tanh), Dense(hidden_dim, output_dim, tanh))
     sparams = symbolicparameters(c)
     ps = initialparameters(c, T) |> NeuralNetworkParameters
-    @variables sinput[1:input_dim] soutput[1:output_dim]
+    @variables sinput[1:input_dim]
     sout = norm(c(sinput, sparams)) ^ 2
     sdparams = symbolic_differentials(sparams)
-    _sgrad = symbolic_gradient(sout, sdparams)
+    _sgrad = symbolic_derivative(sout, sdparams)
     input = rand(T, input_dim, second_dim)
     for k in 1:second_dim
         zgrad = Zygote.gradient(ps -> (norm(c(input[:, k], ps)) ^ 2), ps)[1].params
         for key1 in keys(_sgrad)
             for key2 in keys(_sgrad[key1])
-                executable_gradient = build_nn_function(_sgrad[key1][key2], sinput, soutput, sparams)
-                sgrad = executable_gradient(input, zero(input), ps, k)
+                executable_gradient = _build_nn_function(_sgrad[key1][key2], sparams, sinput)
+                sgrad = executable_gradient(input, ps, k)
                 @test sgrad ≈ zgrad[key1][key2]
             end
         end
@@ -46,10 +46,8 @@ function test_symbolic_gradient2(input_dim::Integer = 3, output_dim::Integer = 1
     input = rand(T, input_dim, second_dim, third_dim)
     zgrad = Zygote.gradient(ps -> (norm(c(input, ps)) ^ 2), ps)[1].params
     sdparams = symbolic_differentials(sparams)
-    _sgrad = symbolic_gradient(sout, sdparams)
-    # soutput is required!
-    @variables soutput[1:1]
-    sgrad = build_executable_gradient(_sgrad, sinput, soutput, sparams)(input, zero(input), ps)
+    _sgrad = symbolic_derivative(sout, sdparams)
+    sgrad = build_nn_function(_sgrad, sparams, sinput)(input, ps)
     for key1 in keys(sgrad) for key2 in keys(sgrad[key1]) @test zgrad[key1][key2] ≈ sgrad[key1][key2] end end
 end
 
@@ -57,6 +55,6 @@ for second_dim in (2, 3, 4)
     test_symbolic_gradient(3, 1, 2, Float64, second_dim)
 end
 
-for (second_dim, third_dim) in ((1, 1), )
-    test_symbolic_gradient2(3, 1, 2, Float64, second_dim, third_dim)
-end
+# for (second_dim, third_dim) in ((1, 1), )
+#     test_symbolic_gradient2(3, 1, 2, Float64, second_dim, third_dim)
+# end
