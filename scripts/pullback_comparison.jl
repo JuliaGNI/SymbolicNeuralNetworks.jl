d8512a30e9e3bfd6e2cb1c0180ee1c24a600899d
benedict-96 <benedikt.brantner@icloud.com>
2024-12-03 16:00:16 +0100

    Added script for comparing pullbacks and added an explanation for why we implemented the pullback the way we did.

---
+++ scripts/pullback_comparison.jl
@@ -0,0 +1,26 @@
+using SymbolicNeuralNetworks
+using AbstractNeuralNetworks
+using GeometricMachineLearning
+using AbstractNeuralNetworks: FeedForwardLoss
+using GeometricMachineLearning: ZygotePullback
+import Random
+Random.seed!(123)
+
+c = Chain(Dense(2, 3, tanh), Dense(3, 1, tanh))
+nn = SymbolicNeuralNetwork(c)
+nn_cpu = NeuralNetwork(c, CPU())
+loss = FeedForwardLoss()
+spb = SymbolicPullback(nn, loss)
+zpb = ZygotePullback(loss)
+
+batch_size = 10000
+input = rand(2, batch_size)
+output = rand(1, batch_size)
+# output sensitivities
+_do = 1.
+
+spb(nn_cpu.params, nn.model, (input, output))[2](_do)
+zpb(nn_cpu.params, nn.model, (input, output))[2](_do)
+@time spb_evaluated = spb(nn_cpu.params, nn.model, (input, output))[2](_do)
+@time zpb_evaluated = zpb(nn_cpu.params, nn.model, (input, output))[2](_do)[1].params
+# @assert values(spb_evaluated) .≈ values(zpb_evaluated)
