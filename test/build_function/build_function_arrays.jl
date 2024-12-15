4194d0ba90c6a52f1d78b4fc428a1de071e95dbf
benedict-96 <benedikt.brantner@icloud.com>
2024-12-15 23:35:34 +0100

    Now importing correct structs/functions.
    Fixed typos.

--- test/build_function/build_function_arrays.jl
+++ test/build_function/build_function_arrays.jl
@@ -1,5 +1,5 @@
 using SymbolicNeuralNetworks: build_nn_function, SymbolicNeuralNetwork
-using AbstractNeuralNetworks: Chain, Dense, initialparameters, NeuralNetworkParameters
+using AbstractNeuralNetworks: Chain, Dense, NeuralNetwork
 using Test
 import Random
 Random.seed!(123)
@@ -9,12 +9,12 @@ function build_function_for_array_valued_equation(input_dim::Integer=2, output_d
     nn = NeuralNetwork(ch)
     snn = SymbolicNeuralNetwork(nn)
     eqs = [(a = ch(snn.input, snn.params), b = ch(snn.input, snn.params).^2), (c = ch(snn.input, snn.params).^3, )]
-    funcs = build_nn_function(eqs, nn.params, nn.input)
-    input = [1., 2.]
-    a = ch(input, ps)
-    b = ch(input, ps).^2
-    c = ch(input, ps).^3
-    funcs_evaluated = funcs(input, ps)
+    funcs = build_nn_function(eqs, snn.params, snn.input)
+    input = Vector(1:input_dim)
+    a = ch(input, nn.params)
+    b = ch(input, nn.params).^2
+    c = ch(input, nn.params).^3
+    funcs_evaluated = funcs(input, nn.params)
     funcs_evaluated_as_vector = [funcs_evaluated[1].a, funcs_evaluated[1].b, funcs_evaluated[2].c]
     result_of_standard_computation = [a, b, c]
 
